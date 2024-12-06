d9d198f1de3ff130b7516b3e43b5ccea483d99b2
LynxQuas <lynxquas@gmail.com>
2024-12-06 08:47:09 +0000

    feat: Home page
    Signed-off-by: LynxQuas <lynxquas@gmail.com>

--- apps/event-planning/src/app/page.tsx
+++ apps/event-planning/src/app/page.tsx
@@ -1,3 +1,25 @@
+import HeroSection from "../components/homepage/herosection/HeroSection";
+import BreezeButton from "../ui/BreezeButton";
+import arrowRight from "../icons/arrow-right.svg";
+import Image from "next/image";
+
 export default async function DashboardPage() {
-    return <div>Event Planning Page</div>;
+    return (
+        <main>
+            <div className="flex justify-center mt-[16px] mb-[23px]">
+                <BreezeButton className="flex items-center py-[8px] gap-[8px] px-[24px] shadow-customShadow">
+                    App preview
+                    <Image
+                        alt="arrow icon"
+                        src={arrowRight}
+                        className="w-auto h-auto"
+                    />
+                </BreezeButton>
+            </div>
+
+            <section>
+                <HeroSection />
+            </section>
+        </main>
+    );
 }
