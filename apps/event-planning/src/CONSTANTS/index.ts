6117c56dff90385e3a04782d26f9b7681b4fbb4b
Thet Paing Hein <wiskisec@gmail.com>
2024-12-10 19:18:24 +0630

    fix: style and interest data fetching
    Signed-off-by: Thet Paing Hein <wiskisec@gmail.com>

--- apps/event-planning/src/CONSTANTS/index.ts
+++ apps/event-planning/src/CONSTANTS/index.ts
@@ -1,13 +1,13 @@
-import funAndFood from "../icons/interest-icons/fun_food.svg";
-import artAndDesign from "../icons/interest-icons/art_design.svg";
-import education from "../icons/interest-icons/education.svg";
+// import funAndFood from "../icons/interest-icons/fun_food.svg";
+// import artAndDesign from "../icons/interest-icons/art_design.svg";
+// import education from "../icons/interest-icons/education.svg";
 
-import mirrorBall from "../icons/interest-icons/clubbing.svg";
-import musicAndLife from "../icons/interest-icons/music.svg";
-import socialAndNetworking from "../icons/interest-icons/social_networking.svg";
-import sports from "../icons/interest-icons/sports.svg";
-import technology from "../icons/interest-icons/technology.svg";
-import weekendGateWay from "../icons/interest-icons/weekend_gateway.svg";
+// import mirrorBall from "../icons/interest-icons/clubbing.svg";
+// import musicAndLife from "../icons/interest-icons/music.svg";
+// import socialAndNetworking from "../icons/interest-icons/social_networking.svg";
+// import sports from "../icons/interest-icons/sports.svg";
+// import technology from "../icons/interest-icons/technology.svg";
+// import weekendGateWay from "../icons/interest-icons/weekend_gateway.svg";
 
 import webIcon from "../icons/vector-1.svg";
 import phoneIcon from "../icons/vector-2.svg";
@@ -19,55 +19,10 @@ import linkedInIcon from "../icons/linkedin.svg";
 import tiktokIcon from "../icons/tiktok.svg";
 import telegramIcon from "../icons/telegram.svg";
 
-export const eventCategories = [
-    {
-        image: funAndFood,
-        label: "Fun & Food",
-    },
-
-    {
-        image: artAndDesign,
-        label: "Art & Design",
-    },
-
-    {
-        image: education,
-        label: "Education",
-    },
-
-    {
-        image: technology,
-        label: "Technology",
-    },
-
-    {
-        image: mirrorBall,
-        label: "Clubbing",
-    },
-
-    {
-        image: musicAndLife,
-        label: "Music",
-    },
-    {
-        image: sports,
-        label: "Sports",
-    },
-    {
-        image: socialAndNetworking,
-        label: "Social & Networking",
-    },
-
-    {
-        image: weekendGateWay,
-        label: "Weekend Gateway",
-    },
-];
-
 export const contactInfo = [
     { icon: webIcon, text: "breezemm.com" },
     { icon: phoneIcon, text: "09-443 111 017" },
-    { icon: emailIcon, text: "customerservice@breezemm.com" },
+    { icon: emailIcon, text: "care@breezemm.com" },
 ];
 
 export const socialMedias = [
@@ -77,3 +32,5 @@ export const socialMedias = [
     tiktokIcon,
     telegramIcon,
 ];
+
+export const AUTH_PREFIX = "/auth"
