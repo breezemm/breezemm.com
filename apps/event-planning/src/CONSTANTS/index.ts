3d06f4b66657b21d636f95428373befd7d766eb9
Thet Paing Hein <wiskisec@gmail.com>
2024-12-12 12:22:19 +0630

    fix: module not found errors
    Signed-off-by: Thet Paing Hein <wiskisec@gmail.com>

--- apps/event-planning/src/CONSTANTS/index.ts
+++ apps/event-planning/src/CONSTANTS/index.ts
@@ -1,9 +1,9 @@
-import webIcon from "../icons/vector-1.svg";
-import phoneIcon from "../icons/vector-2.svg";
-import emailIcon from "../icons/email-icon.svg";
+import webIcon from "../icons/contact-icons/web-icon.svg";
+import phoneIcon from "../icons/contact-icons/phone-icon.svg";
+import emailIcon from "../icons/contact-icons/email-icon.svg";
 
 // social media
-import facebookIcon from "../icons/social-media-icons/facebookIcon.svg";
+import facebookIcon from "../icons/social-media-icons/facebook-icon.svg";
 import instagramIcon from "../icons/social-media-icons/instagram.svg";
 import linkedInIcon from "../icons/social-media-icons/linkedin.svg";
 import tiktokIcon from "../icons/social-media-icons/tiktok.svg";
@@ -37,6 +37,56 @@ export const socialMedias = [
     linkedInIcon,
     tiktokIcon,
     telegramIcon,
+];
+
+export const appStoreIcons = [
+    { src: appleAppStoreIcon, alt: "apple app store icon" },
+    { src: googlePlayStoreIcon, alt: "google play store icon" },
+];
+
+export const appNavigation = [
+    { iconSrc: homeIcon, alt: "home icon", href: "" },
+    { iconSrc: searchIcon, alt: "search icon", href: "" },
+    { iconSrc: eventIcon, alt: "event icon", href: "" },
+    { iconSrc: notificationIcon, alt: "notification icon", href: "" },
+    { iconSrc: userProfileIcon, alt: "user profile icon", href: "" },
+];
+
+export const POST_DUMMY_DATA = [
+    {
+        id: "30",
+        name: "breeze PUBLIC Launch",
+        start_date: "1 Mar (Tue)",
+        status: "FREE",
+        description: "breeze is finally OUT OF THE LAB.",
+        image: breezeEventPhoto,
+        user: {
+            id: "11",
+            nickname: "A Chan Pyae",
+            profile_image: aChanPyae,
+            time: "5m",
+        },
+        like_count: "1000",
+        friend_comment: true,
+        comment_count: "68",
+    },
+    {
+        id: "29",
+        name: "ARENA NIGHT",
+        start_date: "16 Mar (Fri)",
+        status: "PAID",
+        description: "",
+        image: arenaEventPhoto,
+        user: {
+            id: "32",
+            nickname: "ARENA Entertainment",
+            time: "20m",
+            profile_image: arenaPhoto,
+        },
+        like_count: "86",
+        comment_count: "100",
+        friend_comment: false,
+    },
 ];
 
 export const AUTH_PREFIX = "auth";
